08f94167fbc20d1f5eec0de1aa05220a00e96c1d
avasyukhin <avasyukhin@inbox.ru>
2016-05-25 10:44:51 +0400

    Refactoring

---
+++ src/main/java/cache/MapCache.java
@@ -0,0 +1,23 @@
+package cache;
+
+
+import annotations.Cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Aphex on 21.05.2016.
+ */
+@Cache(name = "Map")
+public class MapCache implements AbstractCache<Integer, String> {
+    Map<Integer, String> container = new HashMap<Integer, String>();
+
+    public void put(Integer key, String value) {
+        container.put(key, value);
+    }
+
+    public String get(Integer key) {
+        return container.get(key);
+    }
+}
---
+++ src/main/java/cache/SortedMapCache.java
@@ -0,0 +1,22 @@
+package cache;
+
+import annotations.Cache;
+
+import java.util.TreeMap;
+import java.util.SortedMap;
+
+/**
+ * Created by Aphex on 21.05.2016.
+ */
+@Cache(name = "SortedMap")
+public class SortedMapCache implements AbstractCache<Integer, String> {
+    SortedMap<Integer, String> container = new TreeMap<Integer, String>();
+
+    public void put(Integer key, String value) {
+        container.put(key, value);
+    }
+
+    public String get(Integer key) {
+        return container.get(key);
+    }
+}
